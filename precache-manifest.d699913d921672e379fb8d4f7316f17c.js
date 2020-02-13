1a8a0707040d994456758242df52136799aec301
陳品蓉 <chenpinjung@chenpinrongde-MacBook-Pro.local>
2020-02-13 23:02:43 +0800

    Updates

---
+++ precache-manifest.d699913d921672e379fb8d4f7316f17c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e87b26b56332ef9cf4e04e63937a27d",
+    "url": "/Mask/index.html"
+  },
+  {
+    "revision": "86f98fc1a7a5502e7247",
+    "url": "/Mask/static/css/main.501c2d74.chunk.css"
+  },
+  {
+    "revision": "4d94aafc7f49e6341769",
+    "url": "/Mask/static/js/2.bcbe1341.chunk.js"
+  },
+  {
+    "revision": "ac5d9a2ccd3ba848587d2a460762f2f0",
+    "url": "/Mask/static/js/2.bcbe1341.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86f98fc1a7a5502e7247",
+    "url": "/Mask/static/js/main.3af32872.chunk.js"
+  },
+  {
+    "revision": "cb669d1f0aa342272296",
+    "url": "/Mask/static/js/runtime-main.1b667238.js"
+  }
+]);
